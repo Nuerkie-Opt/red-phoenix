7cd2b520c248a48a1166f01b7c548e39a795351d
Nuerkie-Opt <nuerkie.20@gmail.com>
2021-06-23 01:21:50 +0000

    Added authentication

---
+++ lib/components/loading.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+loader([s = 40.0]) {
+  return SpinKitWave(
+    color: Color(0xFF721727),
+  );
+}
